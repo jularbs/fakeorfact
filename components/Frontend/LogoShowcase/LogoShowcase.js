896af8811f682a99b48677d6b12314d91a655546
ralph jularbal <ralphjularbal@yahoo.com>
2022-01-18 16:28:40 +0800

    init LogoShowcase

---
+++ components/Frontend/LogoShowcase/LogoShowcase.js
@@ -0,0 +1,19 @@
+import "./styles.scss";
+
+const LogoShowcase = () => {
+  return (
+    <>
+      <div className="logo-showcase-container">
+        <img src="/logos/dzrh-logo-color.svg" />
+        <img src="/logos/rhtv-logo-circle-color.svg" />
+        <img src="/logos/love-radio-color.svg" />
+        <img src="/logos/yesfm-logo-color.svg" />
+        <img src="/logos/easy-logo-color.svg" />
+        <img src="/logos/rn-logo-color.svg" />
+        <img src="/logos/aksyon-logo-color.svg" />
+      </div>
+    </>
+  );
+};
+
+export default LogoShowcase;
