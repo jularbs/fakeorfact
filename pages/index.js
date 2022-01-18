907413b627fe48866c1ed0a2e00437da68a29229
ralph jularbal <ralphjularbal@yahoo.com>
2022-01-18 18:28:28 +0800

    assets

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Link from "next/link";
 // reactstrap components
 import { Row, Col } from "reactstrap";
 // core components
@@ -7,16 +6,8 @@ import HeroBranding from "components/Frontend/HeroBranding";
 import Showcase from "components/Frontend/Showcase";
 import HeroMetrics from "components/Frontend/HeroMetrics";
 import GenericCTA from "components/Frontend/GenericCTA";
-import SideBySide from "components/Frontend/SideBySide";
-import HeroBasic from "components/Frontend/HeroBasic";
-import ContactUsSection from "components/Frontend/Sections/ContactUsSection";
-import OrgChartSection from "components/Frontend/Sections/OrgChartSection";
-import OurBusinessesSection from "components/Frontend/Sections/OurBusinessesSection";
-import GenericCard from "components/Frontend/GenericCard";
-import StoryCard from "components/Frontend/StoryCard";
-import HeroBeforeAfter from "components/Frontend/HeroBeforeAfter";
+import LogoShowcase from "components/Frontend/LogoShowcase/LogoShowcase";
 import Navigation from "components/Frontend/Navigation";
-import FrontendHeader from "../components/Headers/FrontendHeader";
 import { useRef } from "react";
 
 function Index() {
@@ -28,44 +19,12 @@ function Index() {
   return (
     <>
       <Navigation />
-      {/* <FrontendHeader /> */}
       <div className="main">
-        {/* <HeroBeforeAfter next={heroBrandingRef} /> */}
         <HeroBranding ref={heroBrandingRef} next={showcaseRef} />
-        {/* <StoryCard ref={storyCardRef} /> */}
-        {/* <HeroBasic next={showcaseRef} /> */}
         <Showcase ref={showcaseRef} next={heroMetricsRef} />
         <HeroMetrics ref={heroMetricsRef} next={genericCtaRef} />
         <GenericCTA ref={genericCtaRef} />
-        {/* <SideBySide /> */}
-        {/* <SideBySide reverse /> */}
-        {/* <OrgChartSection /> */}
-        {/* <ContactUsSection /> */}
-        {/* <OurBusinessesSection /> */}
-        {/* <div className="posts-section" style={{ overflowX: "hidden" }}>
-          <Row className="px-3">
-            <Col lg={3}>
-              <GenericCard />
-            </Col>
-            <Col lg={3}>
-              <GenericCard />
-            </Col>
-            <Col lg={3}>
-              <GenericCard />
-            </Col>
-            <Col lg={3}>
-              <GenericCard />
-            </Col>
-          </Row>
-          <Row className="px-3">
-            <Col lg={6}>
-              <GenericCard />
-            </Col>
-            <Col lg={6}>
-              <GenericCard />
-            </Col>
-          </Row>
-        </div> */}
+        <LogoShowcase />
       </div>
     </>
   );
