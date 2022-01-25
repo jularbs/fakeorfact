64887ce3998033ef2230a320ce51c60d1027cc2b
ralph jularbal <ralphjularbal@yahoo.com>
2022-01-25 22:00:43 +0800

    layouts

--- components/Frontend/StoryCard/exp.js
+++ components/Frontend/StoryCard/exp.js
@@ -4,16 +4,7 @@ import { IoChevronBackOutline, IoChevronForwardOutline } from "react-icons/io5";
 import { forwardRef } from "react";
 import { Row, Col } from "reactstrap";
 
-const StoryCard = forwardRef(({ next, realData, timeline }, myRef) => {
-  const data = {
-    bgLocation:
-      "https://images.unsplash.com/photo-1578593139775-971441c3c518?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2690&q=80",
-    imgLocation: "/logos/yes-logo-old.svg",
-    label: "1930's",
-    title: "RH Goes Television",
-    excerpt:
-      "And anyways, as Cecil Adams reasoned, “[Do you really] think graphic arts supply houses were hiring classics scholars in the 1960s?” Perhaps. But it seems reasonable to imagine that there was a version in use far before the age of Letraset. McClintock wrote to Before & After to explain his discovery.",
-  };
+const StoryCard = forwardRef(({ next, data, timeline }, myRef) => {
   const { before, nextLabel, futures, current } = timeline;
 
   const scrollNext = () => {
--- pages/our-company/index.js
+++ pages/our-company/index.js
@@ -34,16 +34,33 @@ function HistoryPage() {
         />
         <StoryCard
           ref={timeline1}
-          next={timeline2}
+          next={timeline4}
+          data={{
+            bgLocation:
+              "https://images.unsplash.com/photo-1578593139775-971441c3c518?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2690&q=80",
+            title: "RH goes on air",
+            label: "1930's",
+            excerpt:
+              "Manila Broadcasting Company traces its roots back to “the Voice of the Philippines” – KZRH, the oldest radio station in the Philippines which first went on the air back in June 15, 1939. \n\n Formerly owned and operated by H.E. Heacock, KZRH was then a 1,000 watt radio station along Escolta, the bustling financial district of pre-war Manila. KZRH was so successful that within after less than two years operation, it opened a sister station in Cebu City – DYRC.",
+          }}
           timeline={{
             current: "1930's",
             nextLabel: "1940's",
             futures: ["1950's - 1960's", "1970's", "1980's"],
           }}
         />
-        <StoryCard
+        {/* <StoryCard
           ref={timeline2}
           next={timeline3}
+          data={{
+            bgLocation:
+              "https://images.unsplash.com/photo-1578593139775-971441c3c518?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2690&q=80",
+            imgLocation: "/logos/yes-logo-old.svg",
+            title: "RH goes on air",
+            label: "1940's",
+            excerpt:
+              "Manila Broadcasting Company traces its roots back to “the Voice of the Philippines” – KZRH, the oldest radio station in the Philippines which first went on the air back in June 15, 1939. \n\n Formerly owned and operated by H.E. Heacock, KZRH was then a 1,000 watt radio station along Escolta, the bustling financial district of pre-war Manila. KZRH was so successful that within after less than two years operation, it opened a sister station in Cebu City – DYRC.",
+          }}
           timeline={{
             before: ["1930's"],
             current: "1940's",
@@ -54,16 +71,34 @@ function HistoryPage() {
         <StoryCard
           ref={timeline3}
           next={timeline4}
+          data={{
+            bgLocation:
+              "https://images.unsplash.com/photo-1578593139775-971441c3c518?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2690&q=80",
+            imgLocation: "/logos/yes-logo-old.svg",
+            title: "RH goes on air",
+            label: "1950's - 1960's",
+            excerpt:
+              "Manila Broadcasting Company traces its roots back to “the Voice of the Philippines” – KZRH, the oldest radio station in the Philippines which first went on the air back in June 15, 1939. \n\n Formerly owned and operated by H.E. Heacock, KZRH was then a 1,000 watt radio station along Escolta, the bustling financial district of pre-war Manila. KZRH was so successful that within after less than two years operation, it opened a sister station in Cebu City – DYRC.",
+          }}
           timeline={{
             before: ["1940's"],
             current: "1950's - 1960's",
             nextLabel: "1970's",
             futures: ["1980's", "1990's"],
           }}
-        />
+        /> */}
         <StoryCard
           ref={timeline4}
-          next={timeline5}
+          next={timeline8}
+          data={{
+            bgLocation:
+              "https://images.unsplash.com/photo-1578593139775-971441c3c518?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2690&q=80",
+            imgLocation: "/logos/yes-logo-old.svg",
+            title: "Expanding into the FM Band",
+            label: "1970's",
+            excerpt:
+              "Manila Broadcasting Company traces its roots back to “the Voice of the Philippines” – KZRH, the oldest radio station in the Philippines which first went on the air back in June 15, 1939. \n\n Formerly owned and operated by H.E. Heacock, KZRH was then a 1,000 watt radio station along Escolta, the bustling financial district of pre-war Manila. KZRH was so successful that within after less than two years operation, it opened a sister station in Cebu City – DYRC.",
+          }}
           timeline={{
             before: ["1950's - 1960's"],
             current: "1970's",
@@ -71,11 +106,20 @@ function HistoryPage() {
             futures: ["1990's", "2000's"],
           }}
         />
-        <StoryCard
+        {/* <StoryCard
           ref={timeline5}
           next={timeline6}
+          data={{
+            bgLocation:
+              "https://images.unsplash.com/photo-1578593139775-971441c3c518?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2690&q=80",
+            imgLocation: "/logos/yes-logo-old.svg",
+            title: "RH goes on air",
+            label: "1980's",
+            excerpt:
+              "Manila Broadcasting Company traces its roots back to “the Voice of the Philippines” – KZRH, the oldest radio station in the Philippines which first went on the air back in June 15, 1939. \n\n Formerly owned and operated by H.E. Heacock, KZRH was then a 1,000 watt radio station along Escolta, the bustling financial district of pre-war Manila. KZRH was so successful that within after less than two years operation, it opened a sister station in Cebu City – DYRC.",
+          }}
           timeline={{
-            before: [ "1970's"],
+            before: ["1970's"],
             current: "1980's",
             nextLabel: "1990's",
             futures: ["2000's", "PRESENT"],
@@ -84,6 +128,15 @@ function HistoryPage() {
         <StoryCard
           ref={timeline6}
           next={timeline7}
+          data={{
+            bgLocation:
+              "https://images.unsplash.com/photo-1578593139775-971441c3c518?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2690&q=80",
+            imgLocation: "/logos/yes-logo-old.svg",
+            title: "RH goes on air",
+            label: "1990's",
+            excerpt:
+              "Manila Broadcasting Company traces its roots back to “the Voice of the Philippines” – KZRH, the oldest radio station in the Philippines which first went on the air back in June 15, 1939. \n\n Formerly owned and operated by H.E. Heacock, KZRH was then a 1,000 watt radio station along Escolta, the bustling financial district of pre-war Manila. KZRH was so successful that within after less than two years operation, it opened a sister station in Cebu City – DYRC.",
+          }}
           timeline={{
             before: ["1970's", "1980's"],
             current: "1990's",
@@ -94,14 +147,32 @@ function HistoryPage() {
         <StoryCard
           ref={timeline7}
           next={timeline8}
+          data={{
+            bgLocation:
+              "https://images.unsplash.com/photo-1578593139775-971441c3c518?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2690&q=80",
+            imgLocation: "/logos/yes-logo-old.svg",
+            title: "RH goes on air",
+            label: "2000's",
+            excerpt:
+              "Manila Broadcasting Company traces its roots back to “the Voice of the Philippines” – KZRH, the oldest radio station in the Philippines which first went on the air back in June 15, 1939. \n\n Formerly owned and operated by H.E. Heacock, KZRH was then a 1,000 watt radio station along Escolta, the bustling financial district of pre-war Manila. KZRH was so successful that within after less than two years operation, it opened a sister station in Cebu City – DYRC.",
+          }}
           timeline={{
             before: ["1970's", "1980's", "1990's"],
             current: "2000's",
             nextLabel: "PRESENT",
           }}
-        />
+        /> */}
         <StoryCard
           ref={timeline8}
+          data={{
+            bgLocation:
+              "https://images.unsplash.com/photo-1578593139775-971441c3c518?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2690&q=80",
+            imgLocation: "/logos/mbc-portait.png",
+            title: "MBC Media Group",
+            label: "PRESENT",
+            excerpt:
+              "Manila Broadcasting Company traces its roots back to “the Voice of the Philippines” – KZRH, the oldest radio station in the Philippines which first went on the air back in June 15, 1939. \n\n Formerly owned and operated by H.E. Heacock, KZRH was then a 1,000 watt radio station along Escolta, the bustling financial district of pre-war Manila. KZRH was so successful that within after less than two years operation, it opened a sister station in Cebu City – DYRC.",
+          }}
           timeline={{
             current: "PRESENT",
             before: ["1980's", "1990's", "2000's"],
